fcc7dc25d87f0a3305ac8311639d80faa3b97761
mkcrouther@gmail.com <33792608+crouther@users.noreply.github.com>
2022-04-22 11:33:27 -0400

    Draft Design: Change Post Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './css/fonts.css';
 
 function App() {
   return (
-    <div>Keeping clawing your way through it.<br></br>Your app is running puurrfectly.<br></br>The font family is paawwfect.<br></br>Meow.</div>
+    <div>Keeping clawing your way through it.<br></br>Your app is running puurrfectly.<br></br>The font family is paawwfect.<br></br>GoGato is the Cat's meow.</div>
   );
 }
 
