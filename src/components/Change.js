c68370dd1b977bd4eedf74acaacce60a69dfb9bd
mkcrouther@gmail.com <33792608+crouther@users.noreply.github.com>
2022-04-27 15:29:19 -0400

    Change Version 1.1

--- src/components/Change.js
+++ src/components/Change.js
@@ -10,12 +10,17 @@ function Change(){
             <div className="blur"></div>
 
             <section className="changeScreen">
-                <h1>Change Post</h1>
+                <h1>Change Message</h1>
 
-                <form action="/edit">
-                    <textarea id="contents" name="contents" rows="5" cols="80">revise text</textarea>
+                <form action="/post" method ="put">
+
+                    <input hidden id="id" name="id"></input>
+                    <input hidden id="userid" name="userid"></input>
+                    <input hidden id="parent_post" name="parent_post"></input>
+
+                    <textarea required id="contents" name="contents" rows="5" cols="80" placeholder="revise text"></textarea>
                     <br></br><br></br>
-                    <input type="submit" value="Submit"></input>
+                    <input type="submit" value="Save"></input>
                     
                 </form>
 
